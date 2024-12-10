d2f2e1a8d032581ff9bc4fbc2a475c67eb85f0ea
panda <duremarkastu@gmail.com>
2024-12-10 07:09:17 +0300

    Initial

---
+++ simpleBooking/src/main/java/com/booking/simpleBooking/SimpleBookingApplication.java
@@ -0,0 +1,13 @@
+package com.booking.simpleBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBookingApplication.class, args);
+	}
+
+}
---
+++ simpleBooking/src/test/java/com/booking/simpleBooking/SimpleBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.simpleBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
