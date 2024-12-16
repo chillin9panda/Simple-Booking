fabb6231cc6395f57694b36ec23c832b86c243e2
panda <duremarkastu@gmail.com>
2024-12-16 07:32:46 +0300

    login page added

---
+++ simpleBooking/src/main/java/com/booking/simpleBooking/LoginController.java
@@ -0,0 +1,13 @@
+package com.booking.simpleBooking;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/")
+	public String home() {
+		return "login";
+	}
+}
