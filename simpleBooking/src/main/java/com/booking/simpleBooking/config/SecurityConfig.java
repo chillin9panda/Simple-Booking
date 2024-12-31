92e0a63cc76ef8418e2d5f888473ef10ebf563ab
Duressa Markos <duremarkastu@gmail.com>
2024-12-31 17:42:43 +0300

    update: removed unnecessary stuff

--- simpleBooking/src/main/java/com/booking/simpleBooking/config/SecurityConfig.java
+++ simpleBooking/src/main/java/com/booking/simpleBooking/config/SecurityConfig.java
@@ -14,10 +14,10 @@ class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(authz -> authz
-            .requestMatchers("/addEmployee", "/api/guests/add").permitAll()
-            .requestMatchers("/css/**", "/images/**", "/js/**").permitAll()
-            .anyRequest().authenticated() // Require authentication for other page
-        )
+        .requestMatchers("/addEmployee").permitAll()
+        .requestMatchers("/css/**", "/images/**", "/js/**").permitAll()
+        .anyRequest().authenticated() // Require authentication for other page
+    )
         .formLogin(form -> form
             .permitAll());
 
