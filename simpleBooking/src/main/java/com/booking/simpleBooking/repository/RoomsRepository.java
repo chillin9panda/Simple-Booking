e80135ee52ae9e1abf0ec380020671af71d264ec
panda <duremarkastu@gmail.com>
2025-01-13 22:19:49 +0300

    Revert "Merge pull request #4 from junckerpoi/rooms"
    This reverts commit 17a370e4a7c0ca13de5fbfd904266faa7f101b07, reversing
    changes made to 5fd1196f55ad401096b776770804d6ee43594ab1.
    
    reverts

--- simpleBooking/src/main/java/com/booking/simpleBooking/repository/RoomsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.booking.simpleBooking.repository;
-
-import com.booking.simpleBooking.model.Rooms;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuestsRepository extends JpaRepository <Rooms, Int> {
-
-}
