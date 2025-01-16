ec1a3916bc395ee1d51ac9c9ec9e4f0243111721
panda <duremarkastu@gmail.com>
2025-01-16 15:43:26 +0300

    booking model/table

---
+++ simpleBooking/src/main/java/com/booking/simpleBooking/model/Booking.java
@@ -0,0 +1,75 @@
+package com.booking.simpleBooking.model;
+
+import java.sql.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Booking {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer bookingID;
+
+  @ManyToOne
+  @JoinColumn(name = "phone_num", referencedColumnName = "phone_num")
+  private Guests guest;
+
+  @OneToOne
+  @JoinColumn(name = "room_number", referencedColumnName = "room_number")
+  private Rooms room;
+
+  private Date checkInDate;
+  private Date checkOutDate;
+
+  private Boolean isActive;
+
+  // getter and setters
+  public void setGuest(Guests guest) {
+    this.guest = guest;
+  }
+
+  public Guests getGuest() {
+    return guest;
+  }
+
+  public void setRoom(Rooms room) {
+    this.room = room;
+  }
+
+  public Rooms getRoom() {
+    return room;
+  }
+
+  public Integer getBookingId() {
+    return bookingID;
+  }
+
+  public void setCheckInDate(Date checkInDate) {
+    this.checkInDate = checkInDate;
+  }
+
+  public Date getCheckInDate() {
+    return checkInDate;
+  }
+
+  public void setCheckOutDate(Date checkOutDate) {
+    this.checkOutDate = checkOutDate;
+  }
+
+  public Date getCheckOutDate() {
+    return checkOutDate;
+  }
+
+  public void setIsActive(Boolean isActive) {
+    this.isActive = isActive;
+  }
+
+  public Boolean getIsActive() {
+    return isActive;
+  }
+}
