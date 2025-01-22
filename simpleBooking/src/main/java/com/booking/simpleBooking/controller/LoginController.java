12782306af2b2cf5d61d0cf882416ba9ce659e5e
Duressa Markos <duremarkastu@gmail.com>
2025-01-22 16:25:49 +0300

    favicon.ico passing removed

--- simpleBooking/src/main/java/com/booking/simpleBooking/controller/LoginController.java
+++ simpleBooking/src/main/java/com/booking/simpleBooking/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.booking.simpleBooking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -10,4 +11,11 @@ public class LoginController {
   // public String LoginPage() {
   // return "login";
   // }
+
+  @GetMapping("/favicon.ico")
+  @ResponseBody
+  public void favicon() {
+
+  }
+
 }
