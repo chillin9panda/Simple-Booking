27467b87af3d75221d777b921f8151b9d362ee5b
panda <duremarkastu@gmail.com>
2025-01-24 20:56:04 +0300

    mariadb case sensetivity for arch

--- simpleBooking/src/main/java/com/booking/simpleBooking/views/BookingView.java
+++ simpleBooking/src/main/java/com/booking/simpleBooking/views/BookingView.java
@@ -12,22 +12,22 @@ import javax.sql.DataSource;
 
 @Component
 public class BookingView {
-    @Autowired
-    private DataSource dataSource;
+  @Autowired
+  private DataSource dataSource;
 
-    @PostConstruct
-    public void createBookingView() {
-        try (Connection connection = dataSource.getConnection();
-                Statement statement = connection.createStatement()) {
-            String createViewQuery = "CREATE OR REPLACE VIEW booking_view AS " +
-                    "SELECT b.booking_id, g.first_name, g.phone_num, b.room_number, " +
-                    "CASE WHEN b.is_active = TRUE THEN 'Active' ELSE 'Inactive' END AS status " +
-                    "FROM Booking b " +
-                    "JOIN Guests g ON b.phone_num = g.phone_num";
+  @PostConstruct
+  public void createBookingView() {
+    try (Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement()) {
+      String createViewQuery = "CREATE OR REPLACE VIEW booking_view AS " +
+          "SELECT b.booking_id, g.first_name, g.phone_num, b.room_number, " +
+          "CASE WHEN b.is_active = 1 THEN 'Active' ELSE 'Inactive' END AS status " +
+          "FROM booking b " +
+          "JOIN guests g ON b.phone_num = g.phone_num";
 
-            statement.executeUpdate(createViewQuery);
-        } catch (SQLException e) {
-            System.err.println("Error Creating View: " + e.getMessage());
-        }
+      statement.executeUpdate(createViewQuery);
+    } catch (SQLException e) {
+      System.err.println("Error Creating View: " + e.getMessage());
     }
+  }
 }
