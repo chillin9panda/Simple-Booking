91d7274aecc0088731c0e387a960a933ec548cc5
panda <duremarkastu@gmail.com>
2025-01-24 20:56:30 +0300

    mapping rerouting

--- simpleBooking/src/main/java/com/booking/simpleBooking/controller/RoomsController.java
+++ simpleBooking/src/main/java/com/booking/simpleBooking/controller/RoomsController.java
@@ -15,9 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
-import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/rooms")
 public class RoomsController {
 
